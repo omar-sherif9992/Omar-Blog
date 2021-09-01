a5b725680bd2bf10f1b76675a31ed6f105007522
omar-sherif9992 <omar.sherif9992@gmail.com>
2021-09-01 06:08:41 +0200

    Upload main-blog for deploying

---
+++ Notification/email_manager.py
@@ -0,0 +1,18 @@
+import os
+import smtplib
+
+
+
+class EmailManager:
+
+    def send_email(self,title:str ,message: str, to_addrs: str, first_name: str, last_name: str):
+        my_email =os.environ.get('MY_EMAIL') # the part after the @ is the identity of my email provider
+        my_password = os.environ.get('MY_PASSWORD')
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            # location of our email provider
+            connection.starttls()  # to secure the email
+            connection.login(user=my_email, password=my_password)  # login to your email
+            connection.sendmail(from_addr=my_email,
+                                to_addrs=to_addrs,
+                                msg=f"Subject: {title}\n\nDear {first_name} {last_name},\n\n\n {message}.")  # sending the email
+            print("Process is Completed 100%")
