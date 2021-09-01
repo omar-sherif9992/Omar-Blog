a21317a461c3a5b900d99048cb121c1fe5f019ff
omar-sherif9992 <omar.sherif9992@gmail.com>
2021-09-01 06:56:38 +0200

    update notification_manager.py

--- Notification/notification_manager.py
+++ Notification/notification_manager.py
@@ -1,19 +1,20 @@
-from decouple import config
 from twilio.rest import Client
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 class NotificationManager:
     # This class is responsible for sending notifications with the deal flight details.
     def send_sms(self,message:str):
-        account_sid = config("ACCOUNT_SID")  # Copied from my Dashboard remove config and write yours as a string
-        auth_token = config("AUTH_TOKEN")  # Copied from my Dashboard  remove config and write yours as a string
+        account_sid = os.environ.get("TWILO_ACCOUNT_SID") # Copied from my Dashboard remove config and write yours as a string
+        auth_token = os.environ.get("TWILO_AUTH_TOKEN")  # Copied from my Dashboard  remove config and write yours as a string
 
         client = Client(account_sid, auth_token)
 
         message_t = client.messages \
             .create(
             body=message,
-            from_=config("FROM"),
-            to=config("TO")
+            from_=os.environ.get("TWILO_TO"),
+            to=os.environ.get("TWILO_FROM")
         )
 
         print(message_t.sid)
